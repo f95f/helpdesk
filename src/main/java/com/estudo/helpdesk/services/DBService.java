170d83c9cf82c5e1d10b8fc5767dc72611853c39
f95f <95felipefernandes@gmail.com>
2023-10-17 01:07:18 -0300

    feat: Adicionada exceção para objeto não encontrado

--- src/main/java/com/estudo/helpdesk/services/DBService.java
+++ src/main/java/com/estudo/helpdesk/services/DBService.java
@@ -23,17 +23,22 @@ public class DBService {
     @Autowired
     private ChamadoRepository chamadoRepository;
     public void instanciarDB(){
-
+        Technician t4 = new Technician(null, "Alguém", "12312312311", "al@mail.com", "12345");
+        Technician t3 = new Technician(null, "Ninguém", "32132132132", "ng@mail.com", "12345");
+        Technician t2 = new Technician(null, "Mafagaphos", "11122233344", "mph@mail.com", "12345");
         Technician t1 = new Technician(null, "Ksakama", "12312312312", "kz@mail.com", "123321");
         t1.addRoles(Profiles.ADMIN);
 
         Client c1 = new Client(null, "Riobaldo", "12345678910", "riob@m.com", "1234");
+        Client c2 = new Client(null, "Teste1", "98735432122", "3a@m.com", "1234");
+        Client c3 = new Client(null, "Teste2", "78945612312", "ga@m.com", "1234");
 
         Chamado ch1 = new Chamado(null, Priorities.MEDIA, Status.ONGOING, "Chamadoh 1", "1 Chamadoh", t1, c1);
+        Chamado ch2 = new Chamado(null, Priorities.URGENTE, Status.DELAYED, "Chamadoh 2", "1 Chamadoh", t3, c2);
 
-        technicianRepository.saveAll(Arrays.asList(t1));
-        clientRepository.saveAll(Arrays.asList(c1));
-        chamadoRepository.saveAll(Arrays.asList(ch1));
+        technicianRepository.saveAll(Arrays.asList(t1, t2, t3, t4));
+        clientRepository.saveAll(Arrays.asList(c1, c2, c3));
+        chamadoRepository.saveAll(Arrays.asList(ch1, ch2));
 
     }
 
